0b50ec7bfbed2c6d10084b06db62f71ce41532d3
Daisuke Shimamoto <diskshima@gmail.com>
2017-08-19 13:50:50 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "globals": {
+    "document": true,
+  },
+  "extends": [
+    "airbnb-base",
+    "plugin:flowtype/recommended"
+  ],
+  "plugins": [
+    "flowtype"
+  ]
+};
